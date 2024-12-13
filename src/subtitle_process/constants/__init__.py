0cd6c8e39d065e789bfecbf45ee9a6adada885ce
Quik Zi Hao <94067315+QuikZiHao@users.noreply.github.com>
2024-12-13 15:17:52 +0800

    Update __init__.py

--- src/subtitle_process/constants/__init__.py
+++ src/subtitle_process/constants/__init__.py
@@ -3,6 +3,6 @@ import os
 
 CROP_DIR = os.path.join("src", "subtitle_process", "temp")
 FRAME_PATH = os.path.join("assets", "frames")
-OCR_LANGUAGE = "ch"
+OCR_LANGUAGE = "chinese_cht" #"ch"
 JSON_DATA_DIR = os.path.join("assets", "result", "json")
 CSV_DATA_DIR = os.path.join("assets", "result", "csv")
